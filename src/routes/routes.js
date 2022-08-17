c157a137099bf8b3753f94f25e64ea50323f6cac
Prado Neto <manoel.prado.neto@accenture.com>
2022-08-17 10:54:56 -0300

    Statement schema - joi validation in the body of create statement route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const AccountController = require('../controllers/AccountController');
 const StatementController = require('../controllers/StatementController');
 const AccountMiddlewares = require('../middlewares/AccountMiddlewares');
 const validateBodyAccount  = require('../support/schema/AccountSchema');
+const validateBodyStatement  = require('../support/schema/StatementSchema');
 
 router.post("/account", validateBodyAccount, (req, res) => AccountController.createAccount(req, res));
 router.get("/all-accounts", (req, res) => AccountController.listAll(req, res));
@@ -10,7 +11,7 @@ router.get("/all-accounts", (req, res) => AccountController.listAll(req, res));
 router.get("/account", (req, res) => AccountController.getAccount(req, res));
 
 router.use(AccountMiddlewares.checkCpfExists);
-router.post("/statement", (req, res) => StatementController.createStatement(req, res));
+router.post("/statement", validateBodyStatement, (req, res) => StatementController.createStatement(req, res));
 router.get("/statement", (req, res) => StatementController.listStatementByCpf(req, res));
 
 module.exports = router;
--- src/support/schema/StatementSchema.js
+++ src/support/schema/StatementSchema.js
@@ -1,11 +1,13 @@
 const Joi = require('joi');
+const EnumTypes = require('../enum/EnumTypes');
 
-module.exports = function validateBodyAccount(req, res, next) {
+module.exports = function validateBodyStatement(req, res, next) {
   const dataBody = req.body;
 
   const schema = Joi.object().keys({
-    name: Joi.string().uppercase().max(60).required(),
-    cpf: Joi.string().regex(/^[0-9]+$/).max(11).required()
+    description: Joi.string().max(60).required(),
+    amount: Joi.number().precision(2).required(),
+    type: Joi.string().valid(EnumTypes.TRANSACTION_ENTRY, EnumTypes.TRANSACTION_OUT).required()
   });
 
   const validate = schema.validate(dataBody);
